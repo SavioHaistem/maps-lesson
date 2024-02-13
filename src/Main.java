d1cb86834fe818af186975a30e877e601cec9ed8
SavioHaistem <savio.dantas.5858@gmail.com>
2024-02-13 20:53:31 -0300

    lesson complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import entities.Students;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Map<Students, Integer> bimester = new HashMap<>();
+        Students student1 = new Students(1,"Savio");
+        Students student2 = new Students(2,"Maria");
+
+        bimester.put(student1,10);
+        bimester.put(student2,10);
+        bimester.put(student1,8);
+        bimester.put(student2,9);
+
+        for (Students student : bimester.keySet()) {
+            System.out.print(student.getName());
+            System.out.print(" ");
+            System.out.print(student.getId());
+            System.out.print(" ");
+            System.out.print(bimester.get(student));
+            System.out.print(" ");
+            System.out.print(student.getClass());
+            System.out.print("\n");
+        }
     }
 }
---
+++ src/entities/Students.java
@@ -0,0 +1,42 @@
+package entities;
+
+import java.util.Objects;
+
+public class Students {
+    private Integer id;
+    private String name;
+
+    public Students(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Students students = (Students) o;
+        return Objects.equals(id, students.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
